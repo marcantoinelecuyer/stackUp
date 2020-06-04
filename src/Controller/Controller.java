eb9fd76ab7d5b0a1fb1c3961d2277e58eec799ae
Marc-Antoine <marcantoine.lecuyer7@gmail.com>
2020-06-04 18:23:48 -0400

    Vpp diagrams updated and controller added.

---
+++ src/Controller/Controller.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import View.*;
+
+public class Controller {
+
+	private View v;
+	private int attribute;
+
+	/**
+	 * 
+	 * @param v
+	 */
+	public Controller(View v) {
+		// TODO - implement Controller.Controller
+		throw new UnsupportedOperationException();
+	}
+
+	public void play() {
+		// TODO - implement Controller.play
+		throw new UnsupportedOperationException();
+	}
+
+}
